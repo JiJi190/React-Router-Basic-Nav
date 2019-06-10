94b36537ccf11eb7109fd3b3580be5720f9de1af
unknown <robertrey1234@gmail.com>
2019-06-10 15:46:13 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import { Route } from 'react-router-dom';
 const App = () => (
   <div>
     <Navigation />
+    <Route path="/" exact component={Home} />
+    <Route path="/about" exact component={About} />
+    <Route path="/Contact" exact component={Contact} />
   </div>
 );
 
